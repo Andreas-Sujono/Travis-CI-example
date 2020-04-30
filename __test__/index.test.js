da644a8dfa6e46f406eedcd2470b968fc1524053
Andreas_Sujono <andreassujono@gmail.com>
2020-04-30 23:32:17 +0800

    add travis.yml

---
+++ __test__/index.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('../app'); //reference to you app.js file
+
+
+test('Testing jest', () => {
+    const sum = (a,b) => a+b
+    expect(sum(2,3)).toBe(5)
+})
+
+test('GET / ', (done) => {
+    request(app)
+        .get('/')
+        .then( res => {
+            expect(res.statusCode).toBe(200)
+            console.log(res.text)
+            done()
+        })
+})
