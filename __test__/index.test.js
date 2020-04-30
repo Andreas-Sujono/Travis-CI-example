e951713a5196012a8f645a8dc9633b353e3c5b1c
Andreas_Sujono <andreassujono@gmail.com>
2020-05-01 00:12:44 +0800

    simulate bad linting

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,27 +1,28 @@
 const request = require('supertest');
-const app = require('../app'); //reference to you app.js file
-
+const app = require('../app'); // reference to you app.js file
 
 test('Testing jest', () => {
-    const sum = (a,b) => a+b
-    expect(sum(2,3)).toBe(5)
-})
+        const sum = (a, b) => a + b;
+        expect(sum(2, 3)).toBe(5);
+});
+
+
+
 
 test('Testing jest 2', () => {
-    const substract = (a,b) => a-b
-    expect(substract(2,3)).toBe(-1)
-})
+        const substract = (a, b) => a - b;
+        expect(substract(2, 3)).toBe(-1);
+});
 
 test('simulate failure', () => {
-    expect(false).toBe(true)
-})
+        expect(false).toBe(true);
+});
 
-test('GET / ', (done) => {
-    request(app)
-        .get('/')
-        .then( res => {
-            expect(res.statusCode).toBe(200)
-            console.log(res.text)
-            done()
-        })
-})
+test('GET / ', done => {
+        request(app)
+                .get('/')
+                .then(res => {
+                        expect(res.statusCode).toBe(200);
+                        done();
+                });
+});
