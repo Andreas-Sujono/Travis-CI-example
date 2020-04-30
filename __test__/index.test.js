c3ffc107cc93e0504508826ba01291d9d495d927
Andreas_Sujono <andreassujono@gmail.com>
2020-04-30 23:52:55 +0800

    update testing

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -4,7 +4,12 @@ const app = require('../app'); //reference to you app.js file
 
 test('Testing jest', () => {
     const sum = (a,b) => a+b
-    expect(sum(2,3)).toBe(6)
+    expect(sum(2,3)).toBe(5)
+})
+
+test('Testing jest 2', () => {
+    const substract = (a,b) => a-b
+    expect(substract(2,3)).toBe(-1)
 })
 
 test('GET / ', (done) => {
